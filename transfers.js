735b45dccf221399839612b64d629c5162ea1276
tony <tonyofbyteball@users.noreply.github.com>
2022-06-07 14:04:06 +0300

    delay recheckOldTransfers to handle new transfers

--- transfers.js
+++ transfers.js
@@ -1097,7 +1097,7 @@ async function start() {
 	await checkUnfinishedClaims();
 	setInterval(checkUnfinishedClaims, (process.env.testnet || process.env.devnet ? 2 : 30) * 60 * 1000); // every half an hour
 
-	await recheckOldTransfers();
+	setTimeout(recheckOldTransfers, 61 * 1000);
 	setInterval(recheckOldTransfers, 3600 * 1000); // every hour, in case gas price was too high when the claim was received, and then it got lower
 
 	await updateMaxAmounts();
