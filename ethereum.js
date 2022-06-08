86aa04423e2e8954f00d99cb9dbcf0b58a8b7d3f
tony <tonyofbyteball@users.noreply.github.com>
2022-06-08 11:45:45 +0300

    manage providers only from provider.js

--- ethereum.js
+++ ethereum.js
@@ -2,6 +2,7 @@
 const { ethers } = require("ethers");
 const conf = require('ocore/conf.js');
 const EvmChain = require('./evm-chain.js');
+const { getProvider } = require("./evm/provider.js");
 const { getAddressBlocks } = require("./etherscan.js");
 
 const etherscan_base_url = process.env.testnet ? 'https://api-rinkeby.etherscan.io/' : 'https://api.etherscan.io';
@@ -16,10 +17,13 @@ class Ethereum extends EvmChain {
 		bCreated = true;
 		
 	//	const provider = new ethers.providers.JsonRpcProvider("http://0.0.0.0:8545"); // default
+		/*
 		const provider = process.env.devnet
 			? new ethers.providers.JsonRpcProvider("http://0.0.0.0:7545") // ganache
 			: new ethers.providers.WebSocketProvider(process.env.testnet ? `https://speedy-nodes-nyc.moralis.io/${conf.moralis_key}/eth/rinkeby/ws` : `https://speedy-nodes-nyc.moralis.io/${conf.moralis_key}/eth/mainnet/ws`);
+		*/
 		//	: ethers.providers.InfuraProvider.getWebSocketProvider(process.env.testnet ? "rinkeby" : "homestead", conf.infura_project_id);
+		const provider = getProvider('Ethereum');
 		super('Ethereum', conf.ethereum_factory_contract_addresses, conf.ethereum_assistant_factory_contract_addresses, provider);
 
 	}
--- evm/provider.js
+++ evm/provider.js
@@ -8,11 +8,16 @@ function getProvider(network, bFree) {
 		return new ethers.providers.JsonRpcProvider("http://0.0.0.0:7545") // ganache
 	switch (network) {
 		case 'Ethereum':
-			return new ethers.providers.InfuraProvider(process.env.testnet ? "rinkeby" : "homestead", conf.infura_project_id);
+			return process.env.devnet
+				? new ethers.providers.JsonRpcProvider("http://0.0.0.0:7545") // ganache
+				: new ethers.providers.WebSocketProvider(process.env.testnet ? `https://speedy-nodes-nyc.moralis.io/${conf.moralis_key}/eth/rinkeby/ws` : `https://speedy-nodes-nyc.moralis.io/${conf.moralis_key}/eth/mainnet/ws`);
+			//	: ethers.providers.InfuraProvider.getWebSocketProvider(process.env.testnet ? "rinkeby" : "homestead", conf.infura_project_id);
+		//	return new ethers.providers.InfuraProvider(process.env.testnet ? "rinkeby" : "homestead", conf.infura_project_id);
+		
 		case 'BSC':
 			return new ethers.providers.WebSocketProvider(process.env.testnet ? `https://speedy-nodes-nyc.moralis.io/${conf.moralis_key}/bsc/testnet/ws` : `https://speedy-nodes-nyc.moralis.io/${conf.moralis_key}/bsc/mainnet/ws`);
-		//	return new ethers.providers.JsonRpcProvider(process.env.testnet ? `https://speedy-nodes-nyc.moralis.io/${conf.moralis_key}/bsc/testnet` : `https://speedy-nodes-nyc.moralis.io/${conf.moralis_key}/bsc/mainnet`);
 		//	return new ethers.providers.JsonRpcProvider(process.env.testnet ? "https://data-seed-prebsc-1-s1.binance.org:8545" : "https://bsc-dataseed.binance.org");
+		
 		case 'Polygon':
 			/*
 			const url = bFree
@@ -20,6 +25,7 @@ function getProvider(network, bFree) {
 				: (process.env.testnet ? `https://polygon-mumbai.infura.io/v3/${conf.infura_project_id}` : `https://polygon-mainnet.infura.io/v3/${conf.infura_project_id}`);
 			return new ethers.providers.JsonRpcProvider(url);
 			*/
+		//	return new ethers.providers.JsonRpcProvider((process.env.testnet ? `https://polygon-mumbai.infura.io/v3/${conf.infura_project_id}` : `https://polygon-mainnet.infura.io/v3/${conf.infura_project_id}`));
 		//	return new ethers.providers.WebSocketProvider(process.env.testnet ? `wss://polygon-mumbai.g.alchemy.com/v2/${conf.alchemy_keys.polygon.testnet}` : `wss://polygon-mainnet.g.alchemy.com/v2/${conf.alchemy_keys.polygon.mainnet}`);
 			return new ethers.providers.WebSocketProvider(process.env.testnet ? `https://speedy-nodes-nyc.moralis.io/${conf.moralis_key}/polygon/mumbai/ws` : `https://speedy-nodes-nyc.moralis.io/${conf.moralis_key}/polygon/mainnet/ws`);
 	}
