f031650c372d587b547f3d54ca70b045b5f87cc8
tony <tonyofbyteball@users.noreply.github.com>
2023-03-10 13:49:38 +0400

    keep ws connection alive while deploying

--- evm/deploy-contracts.js
+++ evm/deploy-contracts.js
@@ -60,6 +60,12 @@ async function deploy() {
 	console.error(`====== my ETH address on ${evmNetwork}: `, process.env.devnet ? await provider.getSigner().getAddress() : ethWallet.address);
 	const signer = process.env.devnet ? provider.getSigner(0) : ethWallet.connect(provider);
 
+	if (provider._websocket && !process.env.devnet) {
+		provider.on('block', (blockNumber) => {
+			console.log('got new block', blockNumber);
+			provider._websocket.ping();
+		});
+	}
 
 
 	async function createEvmOracle() {
