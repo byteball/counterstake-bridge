898baaf62cc69f7332dc735ea376bf88e04951b3
tony <tonyofbyteball@users.noreply.github.com>
2024-01-09 12:58:46 +0400

    fix currency id

--- prices.js
+++ prices.js
@@ -124,7 +124,7 @@ const fetchCoingeckoExchangeRate = async (in_currency, out_currency) => {
 	const id = getCoingeckoId(in_currency.toLowerCase());
 	out_currency = out_currency.toLowerCase();
 	if (!['usd', 'eth', 'bnb'].includes(out_currency))
-		return await fetchExchangeRateCached(id, 'usd') / await fetchExchangeRateCached(out_currency, 'usd');
+		return await fetchExchangeRateCached(in_currency, 'usd') / await fetchExchangeRateCached(out_currency, 'usd');
 	const data = await request(`https://api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=${out_currency}`)
 	if (!data[id] || !data[id][out_currency])
 		throw new Error(`no ${out_currency} in CG response ${JSON.stringify(data)}`);
