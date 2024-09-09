a7ae691ee2b54fcf3db384a81931dcb0568ce12e
tony <tonyofbyteball@users.noreply.github.com>
2024-09-09 13:00:09 +0200

    pooled_assistants : assistants only for defillama

--- webserver.js
+++ webserver.js
@@ -103,7 +103,7 @@ router.get('/pooled_assistants', async (ctx) => {
 
 	ctx.body = {
 		status: 'success',
-		data: responseData
+		data: reqBridgesInfo ? responseData : assistants,
 	};
 
 	//	for (let assistant of assistants)
