e5ad52d8f9d9014186662485a312773d21750a29
tony <tonyofbyteball@users.noreply.github.com>
2026-06-09 18:41:04 +0500

    POL id in CG

--- prices.js
+++ prices.js
@@ -127,7 +127,7 @@ function getCoingeckoId(currency) {
 		case 'gbyte': return 'byteball';
 		case 'eth': return 'ethereum';
 		case 'bnb': return 'binancecoin';
-		case 'matic': return 'polygon-pos';
+		case 'matic': return 'polygon-ecosystem-token';
 		default: return currency;
 	}
 }
